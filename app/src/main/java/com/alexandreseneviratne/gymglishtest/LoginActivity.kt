0b9e89483fdd7ad2e4154d4cd894a5767d3e400a
Alex <alexandre.seneviratne@gmail.com>
2019-06-18 18:38:52 +0200

    Login screen UI set

---
+++ app/src/main/java/com/alexandreseneviratne/gymglishtest/LoginActivity.kt
@@ -0,0 +1,41 @@
+package com.alexandreseneviratne.gymglishtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+
+        viewModel.getState().observe(this, Observer{ updateUI(it) })
+
+        login_button.setOnClickListener {
+            viewModel.login(edittext_username.text.toString(), edittext_password.text.toString())
+            clearLoginUI()
+        }
+    }
+
+    private fun updateUI(state: LoginState) {
+        return when (state.authorizationStatus) {
+            true -> Toast.makeText(this, "Yeah !", Toast.LENGTH_SHORT).show()
+            false -> {
+                Toast.makeText(this, state.errorMessage, Toast.LENGTH_SHORT).show()
+                clearLoginUI()
+            }
+        }
+    }
+
+    private fun clearLoginUI() {
+        edittext_username.text.clear()
+        edittext_password.text.clear()
+    }
+}
---
+++ app/src/main/java/com/alexandreseneviratne/gymglishtest/LoginViewModel.kt
@@ -0,0 +1,31 @@
+package com.alexandreseneviratne.gymglishtest
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Alexandre SENEVIRATNE on 6/18/2019.
+ */
+sealed class LoginState(
+    val authorizationStatus: Boolean,
+    val errorMessage: String = ""
+)
+
+object LoginSuccess: LoginState(true)
+object LoginFailure: LoginState(false, "Login failed: wrong username/password !")
+
+class LoginViewModel: ViewModel() {
+    private val state =  MutableLiveData<LoginState>()
+
+    fun getState(): LiveData<LoginState> = state
+
+    fun login(username: String, password: String) {
+        if (username == "root" && password == "root") {
+            state.value = LoginSuccess
+        } else {
+            state.value = LoginFailure
+        }
+    }
+
+}
